468069ea1b8cc6114e9716ce055c733e0d7c7cab
maneesh <mkchaurasiya99@gmail.com>
2023-06-07 10:27:51 +0530

    initail commit

---
+++ src/main/java/com/my_first_project/jwtdemo/JwtdemoApplication.java
@@ -0,0 +1,13 @@
+package com.my_first_project.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtdemoApplication.class, args);
+	}
+
+}
